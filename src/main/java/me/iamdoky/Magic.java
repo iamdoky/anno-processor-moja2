3b95d69e1e3f27b1de3833879fa3bcc217f67a18
doky <48897815+iamdoky@users.noreply.github.com>
2022-03-07 00:48:24 +0900

    feat : annotation processor : magic moja

--- src/main/java/me/iamdoky/Magic.java
+++ src/main/java/me/iamdoky/Magic.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.ANNOTATION_TYPE)        // TYPE = Interface, Class, Enum 사용가능 
+@Target(ElementType.TYPE)        // TYPE = Interface, Class, Enum 사용가능
 @Retention(RetentionPolicy.SOURCE)
 public @interface Magic {
 
--- src/main/java/me/iamdoky/MagicMojaProcessor.java
+++ src/main/java/me/iamdoky/MagicMojaProcessor.java
@@ -1,18 +1,21 @@
 package me.iamdoky;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 import com.sun.source.tree.Tree;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.Name;
-import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.*;
 import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
+import java.io.IOException;
 import java.util.Set;
 
 @AutoService(Processor.class)
@@ -39,8 +42,32 @@ public class MagicMojaProcessor extends AbstractProcessor {
             } else {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, " Proccesing  " + elementName);
             }
-        }
 
+            TypeElement typeElement = (TypeElement)element;
+            ClassName className = ClassName.get(typeElement);
+
+            MethodSpec pullOut = MethodSpec.methodBuilder("pullOut")
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(String.class)
+                    .addStatement("return $S", "Rabbit!!")
+                    .build();
+
+            TypeSpec magicMoja = TypeSpec.classBuilder("MagicMoja")
+                    .addModifiers(Modifier.PUBLIC)
+                    .addSuperinterface(className)
+                    .addMethod(pullOut)
+                    .build();
+
+            Filer filer = processingEnv.getFiler();
+
+            try {
+                JavaFile.builder(className.packageName(), magicMoja)
+                        .build()
+                        .writeTo(filer);
+            } catch (IOException e) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "FATAL ERROR " + e);
+            }
+        }
         return true;
     }
 }
